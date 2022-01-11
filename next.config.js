36521ec2644b9ca74115f16ab13331fe7d8e6995
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-11 22:49:15 +0100

    ...nextTranslate()

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 //https://nextjs.org/docs/advanced-features/i18n-routing
 //https://www.npmjs.com/package/next-translate działa ze stronami statycznie generowanymi
+const nextTranslate = require('next-translate')
+
+
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en-US', 'pl'],
-    defaultLocale: 'en-US',
-  },
+  // i18n: {
+  //   locales: ['en-US', 'pl'],
+  //   defaultLocale: 'en-US',
+  // },
+  ...nextTranslate(), //bezpośredni import camiast teo u góry
 }
