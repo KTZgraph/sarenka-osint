3fa5102968c86bfd32087230dc8a0133c37a6708
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-11 22:47:27 +0100

    npm i next-translate

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 //https://nextjs.org/docs/advanced-features/i18n-routing
+//https://www.npmjs.com/package/next-translate działa ze stronami statycznie generowanymi
 module.exports = {
   reactStrictMode: true,
   i18n: {
