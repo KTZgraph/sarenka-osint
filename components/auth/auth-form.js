8735079a858fc54ce433d50ca849795deece1ea6
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-18 23:34:23 +0100

    hamurger menu w glownym

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,6 +1,26 @@
-function AuthForm() {
+import { useState, useRef } from "react";
 
-    return <h1>Auth</h1>
+import classes from "./auth-form.module.css";
+
+function AuthForm() {
+  const [isLogin, setIsLogin] = useState(false);
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  return (
+    <section className={classes.section}>
+      <form className={classes.form}>
+        <div className={classes.auth}>
+          <label htmlFor="username">Your Username</label>
+          <input type="text" id="username" required ref={usernameInputRef} />
+        </div>
+
+        <div className={classes.action}>
+          <button>Login</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default AuthForm;
