440e000c8a436dc6044f2f88deee103dfff6e89c
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-20 21:52:54 +0100

    formularz rejestracji

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -6,17 +6,36 @@ function AuthForm() {
   const [isLogin, setIsLogin] = useState(false);
   const usernameInputRef = useRef();
   const passwordInputRef = useRef();
+  const passwordConfirmationInputRef = useRef();
 
   return (
-    <section className={classes.section}>
-      <form className={classes.form}>
-        <div className={classes.auth}>
+    <section className={classes.auth}>
+      <h1>Sign up</h1>
+      <form>
+        <div className={classes.control}>
           <label htmlFor="username">Your Username</label>
           <input type="text" id="username" required ref={usernameInputRef} />
         </div>
-
-        <div className={classes.action}>
-          <button>Login</button>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="passwordConfirmation">Confirm Your Password</label>
+          <input
+            type="password"
+            id="passwordConfirmation"
+            required
+            ref={passwordConfirmationInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>Create new account</button>
         </div>
       </form>
     </section>
