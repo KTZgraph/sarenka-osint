6e2ee26f3222d93e4be2e46dce3f1afb673ca474
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-23 22:48:28 +0100

    dodawanie do bazy Postman

--- pages/api/cve/index.js
+++ pages/api/cve/index.js
@@ -9,6 +9,18 @@ async function handler(req, res) {
 
   if (req.method === "POST") {
       console.log("\n\n\n\n\nPOST")
+      const {
+        id,
+        cweID,
+        description,
+        published,
+        updated,
+        vector,
+        baseScore,
+        status,
+        hyperlink,
+        source,
+      } = req.body;
 
     // // połączenie z bazą danych
     const client = await connectToDatabase();
@@ -16,16 +28,16 @@ async function handler(req, res) {
 
     //zwraca promsia
     const result = db.collection("cve").insertOne({
-        id: "3",
-        cweID: "CWE-94",
-        description: "Windows Security Center API Remote Code Execution Vulnerability.",
-        published: "01/11/2022",
-        updated: "01/18/2022",
-        vector: "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-        baseScore: "9.8",
-        status: "critical",
-        hyperlink: "https://portal.msrc.microsoft.com/en-US/security-guidance/advisory/CVE-2022-21874",
-        source: "Microsoft Corporation"
+        id,
+        cweID,
+        description,
+        published,
+        updated,
+        vector,
+        baseScore,
+        status,
+        hyperlink,
+        source,
     });
 
 
