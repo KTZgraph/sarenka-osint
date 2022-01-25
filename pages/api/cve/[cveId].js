698bf184d7279359ffa0281f74d5be245f5507ec
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-25 16:16:51 +0100

    CVE not found

--- pages/api/cve/[cveId].js
+++ pages/api/cve/[cveId].js
@@ -1,17 +1,23 @@
-import {connectToDatabase, getAllDocuments} from '../../../lib/db';
+import { connectToDatabase, getAllDocuments } from "../../../lib/db";
 
-async function handler(req, res){
-    if (req.method !== 'GET'){
-        res.status(405).json({ message: "Method no allowed" });
-        return;
-    }
-    const {cveId} = req.query;
-    // const client = await connectToDatabase();
-    // const cveList = await getAllDocuments(client, "cve", {}, {});
-    res.status(200).json({message: cveId})
+async function handler(req, res) {
+  if (req.method !== "GET") {
+    res.status(405).json({ message: "Method no allowed" });
+    return;
+  }
+  const { cveId } = req.query;
 
+  const client = await connectToDatabase();
+  const cve = await getAllDocuments(client, "cve", {}, { id: cveId })[0];
+  console.log("\n\n\n\n\n");
+  console.log(cve);
 
+  if (cve) {
+    res.status(200).json(cve);
+    return;
+  }
 
+  res.status(404).json({ message: "CVE not found" });
 }
 
 export default handler;
