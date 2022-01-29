1467e3b45cc1c60dc825289bc7780b5b21fb4cda
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-30 00:03:09 +0100

    tłumaczenia i zmienne w tekscie

--- components/search/search.js
+++ components/search/search.js
@@ -10,6 +10,7 @@ function Search() {
   return (
     <div className={classes.search}>
       <h2>{t('search:search')}</h2>
+      <p>{t('search:introduction', {name:'Losoweimię', age:99999999999})}</p>
       <div className={classes.searchItems}>
         <SearchForm />
         {/* searchdata */}
