f704d31846e2995eede9f9414ac92c027c33c280
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-29 17:54:27 +0100

    NotificationContextProvider opakowanie cale layout

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,29 @@ import { Provider } from "next-auth/client";
 
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
-      <Head>
-        <title>Sarenka</title>
-        <meta name="description" content="sarenka osint" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-        <Notification title="test" message="this is a message" status="error"/>
-      </Layout>
+      <NotificationContextProvider>
+        <Head>
+          <title>Sarenka</title>
+          <meta name="description" content="sarenka osint" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+          <Notification
+            title="test"
+            message="this is a message"
+            status="error"
+          />
+        </Layout>
+      </NotificationContextProvider>
     </Provider>
   );
 }
--- store/notification-context.js
+++ store/notification-context.js
@@ -1,10 +1,20 @@
 import { createContext } from "react";
 
-const notificationContext = createContext({
+// nazwa z wielkiej żeby było wiadomo, że można Providera wyciągnąć
+const NotificationContext = createContext({
   notification: null, //{title, messsage, status}
   showNotification: function () {}, //inicjalizacyjnie pusta punckja
   hideNotification: function () {},
 });
 
+export function NotificationContextProvider(props) {
+    // ta funkcją opakowywać komponenty, które powinny mieć dostęp do tego kontekstu
+    // będzie ogarnaić wszystkiecontext-realteted state - useState etc żeby pokazywać i chować tutaj notyfikacje
+  return (
+    <NotificationContext.Provider>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
 
-export default notificationContext;
+export default NotificationContext;
