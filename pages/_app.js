6289ddbbb60e5f6132dfd01467cb687258d6879f
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-29 17:36:32 +0100

    dodanie notyfikacji na sztywno

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 // pages/credentials - optymalizacja nadmiarowych jakiś requestów - dobra praktyka
 // reszta manualnie może sprawdzić sesję
 import Head from "next/head";
+import Notification from "../components/ui/notification";
 import { Provider } from "next-auth/client";
 
 import Layout from "../components/layout/layout";
@@ -12,11 +13,12 @@ function MyApp({ Component, pageProps }) {
     <Provider session={pageProps.session}>
       <Head>
         <title>Sarenka</title>
-        <meta name='description' content='sarenka osint'/>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+        <meta name="description" content="sarenka osint" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Layout>
         <Component {...pageProps} />
+        <Notification title="test" message="this is a message" status="pending"/>
       </Layout>
     </Provider>
   );
