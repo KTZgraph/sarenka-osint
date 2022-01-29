d2c5105a260c0a0c63db7700d044390b3e09adb8
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-29 17:36:53 +0100

    formatiowanei pliku

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    //gdy jesteśmy w developerskim serwerze
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: "sarenka-app-demo",
+        mongodb_password: "BardzoSlabeHaslo123",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "sarenka-osint-demo",
+      },
+    };
+  }
+
+  // gdy jestesmy w produckji
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: "sarenka-app",
+      mongodb_password: "Rm2CyQ0NxW296oxu",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "sarenka-osint",
+    },
+  };
+};
