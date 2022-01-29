60d593361b1961009600fd0334c26ba235822236
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-29 21:03:45 +0100

    404 page

---
+++ components/ui/not-found.js
@@ -0,0 +1,12 @@
+import Button from './button';
+import classes from './not-found.module.css'
+
+function NotFound(){
+return <section className={classes.section}>
+    <h1>404</h1>
+    <p>This page could not be found.</p>
+    <Button link={'/'}>Home</Button>
+</section>
+}
+
+export default NotFound;
--- pages/api/cwe/[cweId].js
+++ pages/api/cwe/[cweId].js
@@ -10,6 +10,9 @@ async function handler(req, res) {
   }
 
   const cweId = req.query.cweId;
+  console.log("\n\n\n\n\n")
+  console.log("cweId")
+  console.log(cweId)
 
   const client = await connectToDatabase();
   const cwe = await getAllDocuments(client, COLLECTION_NAME, {}, { id: cweId });
