7db8128ea89adc41cecd143589fb2f7284558d17
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-30 16:40:00 +0100

    style do zmiany języków

--- components/layout/language-switcher.js
+++ components/layout/language-switcher.js
@@ -6,16 +6,13 @@ import classes from "./language-switcher.module.css";
 
 function LanguageSwitcher() {
   let router = useRouter();
-
-const changeLanguageHandler = (locale) => {
-  console.log(locale)
-}
+  console.log(router.locale)
 
   return (
     <div className={classes.languages}>
         {router.locales.map((locale) => (
           <Link href={router.asPath} locale={locale} key={locale}>
-            <a className={classes.btn}>{locale}</a>
+            <a className={`${locale === router.locale ? [classes.active, classes.btn].join(' ') : classes.btn}`}>{locale}</a>
           </Link>
         ))}
     </div>
