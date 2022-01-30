33a0168d46797c9465dee859a0b3b0b75e33a328
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-30 16:56:32 +0100

    /credentials chroniony link w menu

--- components/layout/main-menu.js
+++ components/layout/main-menu.js
@@ -4,22 +4,20 @@ import { useSession } from "next-auth/client";
 import classes from "./main-menu.module.css";
 import AppImage from "./app-image";
 
-
 function MainMenu() {
   const [session, loading] = useSession();
-
 
   return (
     <nav className={classes.sidebar}>
       {/* Hamburger menu */}
       <div className={classes.hamburgerMenu}>
-          <div className={[classes.line, classes.line1].join(" ")}></div>
-          <div className={[classes.line, classes.line2].join(" ")}></div>
-          <div className={[classes.line, classes.line3].join(" ")}></div>
+        <div className={[classes.line, classes.line1].join(" ")}></div>
+        <div className={[classes.line, classes.line2].join(" ")}></div>
+        <div className={[classes.line, classes.line3].join(" ")}></div>
       </div>
       {/* logo aplikacji */}
-      {session && !loading && <AppImage userEmail={session.user.email}/>}
-      {(!session || loading )&& <AppImage/>}
+      {session && !loading && <AppImage userEmail={session.user.email} />}
+      {(!session || loading) && <AppImage />}
       <ul className={classes.list}>
         <li className={classes.item}>
           <Link href="/">
@@ -44,6 +42,13 @@ function MainMenu() {
             </a>
           </Link>
         </li>
+
+        {/* link widoczny tylko dla zalogowanych */}
+        {session && (
+          <li className={classes.item}>
+            <Link href="/credentials">Credentials</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
